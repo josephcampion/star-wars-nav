acbf15de85a2577f425d945317d6636211a4997c
Joey Campion <josephcampion@fastmail.com>
2025-12-07 14:44:07 -0500

    Add linear and angular figures to sim plotter

--- plotter.py
+++ plotter.py
@@ -20,11 +20,12 @@ class Plotter:
     def plot_sim(self):
         self.plot_lon_axis()
         self.plot_lat_axis()
+        self.plot_lin_axis()
+        self.plot_ang_axis()
         plt.show()
         return
 
     def plot_lon_axis(self):
-
         fig_lon, ax_lon = plt.subplots(4,2)
 
         ax_lon[0,0].plot(self._t, self._X[:,8]) # w
@@ -47,17 +48,17 @@ class Plotter:
         ax_lon[2,1].set_title(r"$q\approx\dot{\theta}$")
         ax_lon[3,1].set_title(r"$\dot{q}$")
 
-        ax_lon[0,0].set_ylabel("[m/s]") # w
-        ax_lon[1,0].set_ylabel("[m/s^2]") # wdot
-        ax_lon[2,0].set_ylabel("[m/s]") # u
-        ax_lon[3,0].set_ylabel("[m/s^2]") # udot
+        ax_lon[0,0].set_ylabel("[m/s]")
+        ax_lon[1,0].set_ylabel("[m/s^2]")
+        ax_lon[2,0].set_ylabel("[m/s]")
+        ax_lon[3,0].set_ylabel("[m/s^2]")
 
-        ax_lon[0,1].set_ylabel("[rad]") # alpha
-        ax_lon[1,1].set_ylabel("[rad]") # theta
-        ax_lon[2,1].set_ylabel("[rad/s]") # q
-        ax_lon[3,1].set_ylabel("[rad/s^2]") # qdot
+        ax_lon[0,1].set_ylabel("[rad]")
+        ax_lon[1,1].set_ylabel("[rad]")
+        ax_lon[2,1].set_ylabel("[rad/s]")
+        ax_lon[3,1].set_ylabel("[rad/s^2]")
 
-        
+        # Add gridlines
         for i in range(4):
             for j in range(2):
                 ax_lon[i,j].grid(True)
@@ -72,15 +73,15 @@ class Plotter:
 
         fig_lat, ax_lat = plt.subplots(4,2)
 
-        ax_lat[0,0].plot(self._t, self._X[:,7]) # v
-        ax_lat[1,0].plot(self._t, self._Xdot[:,7]) # vdot
-        ax_lat[2,0].plot(self._t, self._X[:,11]) # r
-        ax_lat[3,0].plot(self._t, self._Xdot[:,11]) # rdot
+        ax_lat[0,0].plot(self._t, self._X[:,7])
+        ax_lat[1,0].plot(self._t, self._Xdot[:,7])
+        ax_lat[2,0].plot(self._t, self._X[:,11])
+        ax_lat[3,0].plot(self._t, self._Xdot[:,11])
 
-        ax_lat[0,1].plot(self._t, self._X[:,7]) # beta ~= v/V0
-        ax_lat[1,1].plot(self._t, self._X[:,3]) # phi
-        ax_lat[2,1].plot(self._t, self._X[:,9]) # p
-        ax_lat[3,1].plot(self._t, self._Xdot[:,9]) # pdot
+        ax_lat[0,1].plot(self._t, self._X[:,7])
+        ax_lat[1,1].plot(self._t, self._X[:,3])
+        ax_lat[2,1].plot(self._t, self._X[:,9])
+        ax_lat[3,1].plot(self._t, self._Xdot[:,9])
 
         ax_lat[0,0].set_title(r"$v$")
         ax_lat[1,0].set_title(r"$\dot{v}$")
@@ -92,17 +93,17 @@ class Plotter:
         ax_lat[2,1].set_title(r"$p\approx\dot{\phi}$")
         ax_lat[3,1].set_title(r"$\dot{p}$")
 
-        ax_lat[0,0].set_ylabel("[m/s]") # v
-        ax_lat[1,0].set_ylabel("[m/s^2]") # vdot
-        ax_lat[2,0].set_ylabel("[m/s]") # r
-        ax_lat[3,0].set_ylabel("[m/s^2]") # rdot
+        ax_lat[0,0].set_ylabel("[m/s]")
+        ax_lat[1,0].set_ylabel("[m/s^2]")
+        ax_lat[2,0].set_ylabel("[m/s]")
+        ax_lat[3,0].set_ylabel("[m/s^2]")
 
-        ax_lat[0,1].set_ylabel("[rad]") # beta
-        ax_lat[1,1].set_ylabel("[rad]") # phi
-        ax_lat[2,1].set_ylabel("[rad/s]") # p
-        ax_lat[3,1].set_ylabel("[rad/s^2]") # pdot
+        ax_lat[0,1].set_ylabel("[rad]")
+        ax_lat[1,1].set_ylabel("[rad]")
+        ax_lat[2,1].set_ylabel("[rad/s]")
+        ax_lat[3,1].set_ylabel("[rad/s^2]")
 
-        
+        # Add gridlines
         for i in range(4):
             for j in range(2):
                 ax_lat[i,j].grid(True)
@@ -111,4 +112,106 @@ class Plotter:
         ax_lat[3,0].set_xlabel("Time [s]")
         ax_lat[3,1].set_xlabel("Time [s]")
 
+        return
+    
+    def plot_lin_axis(self): # TODO: Add derived states, like vn
+
+        fig_lat, ax_lin = plt.subplots(3,3)
+
+        ax_lin[0,0].plot(self._t, self._X[:,0])
+        ax_lin[1,0].plot(self._t, self._X[:,6])
+        ax_lin[2,0].plot(self._t, self._Xdot[:,6])
+
+        ax_lin[0,1].plot(self._t, self._X[:,1])
+        ax_lin[1,1].plot(self._t, self._X[:,7])
+        ax_lin[2,1].plot(self._t, self._Xdot[:,7])
+
+        ax_lin[0,2].plot(self._t, self._X[:,2])
+        ax_lin[1,2].plot(self._t, self._X[:,8])
+        ax_lin[2,2].plot(self._t, self._Xdot[:,8])
+
+        ax_lin[0,0].set_title(r"$p_n$")
+        ax_lin[1,0].set_title(r"$u$")
+        ax_lin[2,0].set_title(r"$\dot{u}$")
+
+        ax_lin[0,1].set_title(r"$p_e$")
+        ax_lin[1,1].set_title(r"$v$")
+        ax_lin[2,1].set_title(r"$\dot{v}$")
+
+        ax_lin[0,2].set_title(r"$p_d$")
+        ax_lin[1,2].set_title(r"$w$")
+        ax_lin[2,2].set_title(r"$\dot{w}$")
+
+        ax_lin[0,0].set_ylabel("[m]")
+        ax_lin[1,0].set_ylabel("[m/s]")
+        ax_lin[2,0].set_ylabel("[m/s^2]")
+
+        ax_lin[0,1].set_ylabel("[m]")
+        ax_lin[1,1].set_ylabel("[m/s]")
+        ax_lin[2,1].set_ylabel("[m/s^2]")
+
+        ax_lin[0,2].set_ylabel("[m]")
+        ax_lin[1,2].set_ylabel("[m/s]")
+        ax_lin[2,2].set_ylabel("[m/s^2]")
+        
+        # Add gridlines
+        for i in range(3):
+            for j in range(3):
+                ax_lin[i,j].grid(True)
+        
+        # Label bottom rows x-axes
+        for j in range(3):
+            ax_lin[2,j].set_xlabel("Time [s]")
+        
+        return
+    
+    def plot_ang_axis(self): # TODO: Add derived states, like course
+
+        fig_lat, ax_lin = plt.subplots(3,3)
+
+        ax_lin[0,0].plot(self._t, self._X[:,3])
+        ax_lin[1,0].plot(self._t, self._X[:,9])
+        ax_lin[2,0].plot(self._t, self._Xdot[:,9])
+
+        ax_lin[0,1].plot(self._t, self._X[:,4])
+        ax_lin[1,1].plot(self._t, self._X[:,10])
+        ax_lin[2,1].plot(self._t, self._Xdot[:,10])
+
+        ax_lin[0,2].plot(self._t, self._X[:,5])
+        ax_lin[1,2].plot(self._t, self._X[:,11])
+        ax_lin[2,2].plot(self._t, self._Xdot[:,11])
+
+        ax_lin[0,0].set_title(r"$\phi$")
+        ax_lin[1,0].set_title(r"$p\approx\dot{\phi}$")
+        ax_lin[2,0].set_title(r"$\dot{p}$")
+
+        ax_lin[0,1].set_title(r"$\theta$")
+        ax_lin[1,1].set_title(r"$q\approx\dot{\theta}$")
+        ax_lin[2,1].set_title(r"$\dot{q}$")
+
+        ax_lin[0,2].set_title(r"$\psi$")
+        ax_lin[1,2].set_title(r"$r\approx\dot{\psi}$")
+        ax_lin[2,2].set_title(r"$\dot{r}$")
+
+        ax_lin[0,0].set_ylabel("[rad]")
+        ax_lin[1,0].set_ylabel("[rad/s]")
+        ax_lin[2,0].set_ylabel("[rad/s^2]")
+
+        ax_lin[0,1].set_ylabel("[rad]")
+        ax_lin[1,1].set_ylabel("[rad/s]")
+        ax_lin[2,1].set_ylabel("[rad/s^2]")
+
+        ax_lin[0,2].set_ylabel("[rad]")
+        ax_lin[1,2].set_ylabel("[rad/s]")
+        ax_lin[2,2].set_ylabel("[rad/s^2]")
+        
+        # Add gridlines
+        for i in range(3):
+            for j in range(3):
+                ax_lin[i,j].grid(True)
+        
+        # Label bottom rows x-axes
+        for j in range(3):
+            ax_lin[2,j].set_xlabel("Time [s]")
+        
         return
