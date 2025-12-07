7bdee5e2fcba24a60d24927769d5d27651d6d21b
Joey Campion <josephcampion@fastmail.com>
2025-12-06 19:59:49 -0500

    Add example second order system sim

---
+++ run_sim.py
@@ -0,0 +1,96 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+# import parameters
+
+# Want to break things up into different files(/folders?)
+
+# To start, going to steal parameters from 
+
+# Truth kinematic state
+
+# 
+Tsim = 10.0 # seconds
+dt = 0.01 # timestep
+t0 = 0.0
+
+time = np.arange(t0, Tsim, dt)
+nt = len(time)
+
+# Sim and plot 2nd order system
+
+
+# Initialize vectors
+X = np.zeros(nt)
+Xdot = np.zeros(nt)
+Xddot = np.zeros(nt)
+
+# Initial conditions
+x0 = 0.0
+xdot0 = 0.0
+
+X[0] = x0
+Xdot[0] = xdot0
+
+# Input force
+amp =  3.0
+# w_in = 1.0 # [rad/s]
+# U = amp * np.sin(w_in * time)
+U = amp * np.ones(nt)
+
+# natural freq.
+wn = 1.0 # [rad/s]
+
+# damping ratio
+zeta = 0.6 # []
+
+# xddot + 2*wn*zeta*xdot + wn**2*x = u
+# xddot = u - 2*wn*zeta*xdot - wn**2*x
+
+print()
+
+for i in range(nt):
+
+    t = time[i]
+    x = X[i]
+    xdot = Xdot[i]
+    u = U[i]
+
+    xddot = u - 2 * wn * zeta * xdot - wn**2 * x
+    Xddot[i] = xddot
+
+    if i < (nt-1):
+        Xdot[i+1] = Xdot[i] + xddot * dt
+        X[i+1] = X[i] + xdot * dt
+
+
+### Plot stuff
+    
+fig, axs = plt.subplots(3,1)
+
+# ax.axhline(y=0, color="black", linestyle="--")
+# ax.axhline(y=0.5, color="black", linestyle=":")
+# ax.axhline(y=1.0, color="black", linestyle="--")
+# ax.axvline(color="grey")
+# ax.axline((0, 0.5), slope=0.25, color="black", linestyle=(0, (5, 5)))
+
+axs[0].plot(time, X) # , linewidth=2) # , label=r"$\sigma(t) = \frac{1}{1 + e^{-t}}$")
+axs[1].plot(time, Xdot) # , linewidth=2)
+axs[2].plot(time, Xddot) # , linewidth=2)
+
+axs[0].set_title("Position [m]")
+axs[1].set_title("Velocity [m/s]")
+axs[2].set_title("Acceleration [ms/^2]")
+
+# axs[0].title("Position [m]")
+# axs[1].title("Velocity [m]")
+axs[2].set_xlabel("Time [s]")
+
+# axs[0].set(xlim=(-10, 10), xlabel="t")
+# axs[0].legend(fontsize=14)
+
+axs[0].grid(True)
+axs[1].grid(True)
+axs[2].grid(True)
+
+plt.show()
