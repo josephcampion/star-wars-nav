d837abefa8cb2a9f247135f84864663a54e80cf6
Joey Campion <josephcampion@fastmail.com>
2025-12-07 09:45:01 -0500

    Add parameter file with initial conditions

---
+++ parameters.py
@@ -0,0 +1,21 @@
+
+######################################################################################
+                #   Initial Conditions
+######################################################################################
+
+#   Initial conditions for MAV
+north0 = 0.  # initial north position
+east0 = 0.  # initial east position
+down0 = -100.0  # initial down position
+
+u0 = 25.  # initial velocity along body x-axis
+v0 = 0.  # initial velocity along body y-axis
+w0 = 0.  # initial velocity along body z-axis
+
+phi0 = 0.  # initial roll angle
+theta0 = 0.  # initial pitch angle
+psi0 = 0.  # initial yaw angle
+
+p0 = 0. # initial roll rate
+q0 = 0. # initial pitch rate
+r0 = 0. # initial yaw rate
