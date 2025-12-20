1dc0bfb5179f0ba676cf964fe570b5e7d400e8cf
Joey Campion <josephcampion@fastmail.com>
2025-12-20 10:24:13 -0500

    Plot accelerometer model data too

--- models/sensors.py
+++ models/sensors.py
@@ -14,51 +14,47 @@ class SensorModel:
     def get_sensor_data(self, x_truth):
         return x_truth + self.bias + np.random.normal(0, self.noise_std)
 
-class InertialMeasurementUnit(SensorModel):
-    def __init__(self):
-        super().__init__("Inertial Measurement Unit")
+class InertialMeasurementUnit:
+    def __init__(self, gyro_bias=0.0, gyro_noise_std=0.0, accel_bias=0.0, accel_noise_std=0.0):
+        self._x_gyro = Gyroscope(gyro_bias, gyro_noise_std)
+        self._y_gyro = Gyroscope(gyro_bias, gyro_noise_std)
+        self._z_gyro = Gyroscope(gyro_bias, gyro_noise_std)
 
-    def get_sensor_data(self):
-        return self.sensor_data
-
-class GPS(SensorModel):
-    def __init__(self):
-        super().__init__("GPS")
-
-    def get_sensor_data(self):
-        return self.sensor_data
-
-class AirspeedSensor(SensorModel):
-    def __init__(self):
-        super().__init__("Airspeed Sensor")
-
-    def get_sensor_data(self):
-        return self.sensor_data
-
-class BarometricPressureSensor(SensorModel):
-    def __init__(self):
-        super().__init__("Barometric Pressure Sensor")
-
-    def get_sensor_data(self):
-        return self.sensor_data
-
-class Magnetometer(SensorModel):
-    def __init__(self):
-        super().__init__("Magnetometer")
-
-    def get_sensor_data(self):
-        return self.sensor_data # TODO: Add magnetometer data
 
 class Gyroscope(SensorModel):
     def __init__(self, bias, noise_std):
         super().__init__("Gyroscope", bias=bias, noise_std=noise_std)
 
-class Accelerometer:
+class Accelerometer(SensorModel):
     def __init__(self, bias=0.0, noise_std=0.0):
         super().__init__("Accelerometer", bias=bias, noise_std=noise_std)
 
-    # def get_sensor_data(self, x):
-    #     return x[9:12] + np.random.normal(0, self.gyro_noise_std, 3)
+############### TODO: Implement and test these #######################
 
-    def get_sensor_data(self):
-        return self.sensor_data # TODO: Add accelerometer data
+# class GPS(SensorModel):
+#     def __init__(self):
+#         super().__init__("GPS")
+
+#     def get_sensor_data(self):
+#         return self.sensor_data
+
+# class AirspeedSensor(SensorModel):
+#     def __init__(self):
+#         super().__init__("Airspeed Sensor")
+
+#     def get_sensor_data(self):
+#         return self.sensor_data
+
+# class BarometricPressureSensor(SensorModel):
+#     def __init__(self):
+#         super().__init__("Barometric Pressure Sensor")
+
+#     def get_sensor_data(self):
+#         return self.sensor_data
+
+# class Magnetometer(SensorModel):
+#     def __init__(self):
+#         super().__init__("Magnetometer")
+
+#     def get_sensor_data(self):
+#         return self.sensor_data # TODO: Add magnetometer data
--- models/sensors_test.py
+++ models/sensors_test.py
@@ -19,28 +19,41 @@ nt = len(time)
 p_truth = 1.0 * np.sin(3*time)
 q_truth = 2.0 * np.sin(2*time)
 r_truth = 3.0 * np.sin(1*time)
+
+ax_truth = 1.0 * np.sin(3*time)
+ay_truth = 2.0 * np.sin(2*time)
+az_truth = 3.0 * np.sin(1*time)
 # p_truth = np.ones(nt)
 
 x_gyro_meas = np.zeros(nt)
 y_gyro_meas = np.zeros(nt)
 z_gyro_meas = np.zeros(nt)
 
+x_accel_meas = np.zeros(nt)
+y_accel_meas = np.zeros(nt)
+z_accel_meas = np.zeros(nt)
+
 ####################################################
     #   Run Simulation of Sensor Models
 ####################################################
 
 # Make sensor models
-# x_gyro = sensors.Gyroscope(bias=0.1, noise_std=0.1)
-# y_gyro = sensors.Gyroscope(bias=0.1, noise_std=0.1)
-# z_gyro = sensors.Gyroscope(bias=0.1, noise_std=0.1)
 x_gyro = sensors.Gyroscope(0.1, 0.1)
 y_gyro = sensors.Gyroscope(0.1, 0.1)
 z_gyro = sensors.Gyroscope(0.1, 0.1)
+
+x_accel = sensors.Accelerometer(0.05, 0.2)
+y_accel = sensors.Accelerometer(0.05, 0.2)
+z_accel = sensors.Accelerometer(0.05, 0.2)
 
 for i in range(nt):
     x_gyro_meas[i] = x_gyro.get_sensor_data(p_truth[i])
     y_gyro_meas[i] = y_gyro.get_sensor_data(q_truth[i])
     z_gyro_meas[i] = z_gyro.get_sensor_data(r_truth[i])
+
+    x_accel_meas[i] = x_accel.get_sensor_data(ax_truth[i])
+    y_accel_meas[i] = y_accel.get_sensor_data(ay_truth[i])
+    z_accel_meas[i] = z_accel.get_sensor_data(az_truth[i])
 
 ####################################################
             #   Plot Results
@@ -48,28 +61,54 @@ for i in range(nt):
 
 _, axs = plt.subplots(3,1)
 
-axs[0].plot(time, p_truth, label='Sensor')
+axs[0].plot(time, p_truth, label='Truth')
 axs[0].grid(True)
-axs[0].plot(time, x_gyro_meas, label='Truth')
+axs[0].plot(time, x_gyro_meas, label='Sensor', linestyle=':')
 # axs[0].set_xlabel('Time [s]')
-axs[0].set_ylabel('Angular Velocity [rad/s]')
+axs[0].set_ylabel('[rad/s]')
 axs[0].set_title('Angular Velocity Sensor vs Truth')
 axs[0].legend()
 
-axs[1].plot(time, q_truth, label='Sensor')
+axs[1].plot(time, q_truth, label='Truth')
 axs[1].grid(True)
-axs[1].plot(time, y_gyro_meas, label='Truth')
+axs[1].plot(time, y_gyro_meas, label='Sensor', linestyle=':')
 # axs[1].set_xlabel('Time [s]')
-axs[1].set_ylabel('Angular Velocity [rad/s]')
+axs[1].set_ylabel('[rad/s]')
 # axs[1].set_title('Angular Velocity Sensor vs Truth')
 axs[1].legend()
 
-axs[2].plot(time, r_truth, label='Sensor')
+axs[2].plot(time, r_truth, label='Truth')
 axs[2].grid(True)
-axs[2].plot(time, z_gyro_meas, label='Truth')
+axs[2].plot(time, z_gyro_meas, label='Sensor', linestyle=':')
 axs[2].set_xlabel('Time [s]')
-axs[2].set_ylabel('Angular Velocity [rad/s]')
+axs[2].set_ylabel('[rad/s]')
 # axs[2].set_title('Angular Velocity Sensor vs Truth')
+axs[2].legend()
+
+_, axs = plt.subplots(3,1)
+
+axs[0].plot(time, ax_truth, label='Truth')
+axs[0].grid(True)
+axs[0].plot(time, x_accel_meas, label='Sensor', linestyle=':')
+# axs[0].set_xlabel('Time [s]')
+axs[0].set_ylabel('[m/s^2]')
+axs[0].set_title('Specific Acceleration Sensor vs Truth')
+axs[0].legend()
+
+axs[1].plot(time, ay_truth, label='Truth')
+axs[1].grid(True)
+axs[1].plot(time, y_accel_meas, label='Sensor', linestyle=':')
+# axs[1].set_xlabel('Time [s]')
+axs[1].set_ylabel('[m/s^2]')
+# axs[1].set_title('Specific Acceleration Sensor vs Truth')
+axs[1].legend()
+
+axs[2].plot(time, az_truth, label='Truth')
+axs[2].grid(True)
+axs[2].plot(time, z_accel_meas, label='Sensor', linestyle=':')
+axs[2].set_xlabel('Time [s]')
+axs[2].set_ylabel('[m/s^2]')
+# axs[2].set_title('Specific Acceleration Sensor vs Truth')
 axs[2].legend()
 
 plt.show()
