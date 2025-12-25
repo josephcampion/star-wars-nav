41389832a476d884b6979cb1979d40b88a0537f4
Joey Campion <josephcampion@fastmail.com>
2025-12-25 13:35:42 -0500

    Add NED to body frame rotation and unit test

--- simulation/kinematics.py
+++ simulation/kinematics.py
@@ -55,3 +55,37 @@ def inertia_mat_inv(Jx, Jy, Jz, Jxz):
         [Jxz/gamma, 0.0,    Jx/gamma]
     ])
 
+class InputAxisError(Exception):
+    pass
+
+def rot(angle_rad, axis):
+
+    ca = np.cos(angle_rad)
+    sa = np.sin(angle_rad)
+
+    if axis == 0: # x
+        return np.array([
+            [1.0,   0.0,    0.0],
+            [0.0,   ca,     sa],
+            [0.0,   -sa,    ca]
+        ])
+    elif axis == 1: # y
+        return np.array([
+            [ca,    0.0,    -sa],
+            [0.0,   1.0,    0.0],
+            [sa,    0.0,    ca]
+        ])
+    elif axis == 2: # z
+        return np.array([
+            [ca,    sa,     0.0],
+            [-sa,   ca,     0.0],
+            [0.0,   0.0,    1.0]
+        ])
+    else:
+        raise InputAxisError("Input axis must be 0, 1, or 2 (for x, y, or z).")
+
+# NOTE: Angles are in radians (since np.sin/cos are too)
+def get_dcm_ned2bod(roll, pitch, yaw):
+    return rot(roll, 0) @ rot(pitch, 1) @ rot(yaw, 2)
+
+
---
+++ simulation/test_kinematics.py
@@ -0,0 +1,55 @@
+
+import numpy as np
+from pytest import approx
+import simulation.kinematics as kt
+
+#---------- Rotation Matrices ------------#
+
+# DCM_phi = rot(phi_rad, 0)
+
+def get_dcm_ned2bod_explicit(roll, pitch, yaw):
+
+    cr = np.cos(roll)
+    sr = np.sin(roll)
+
+    cp = np.cos(pitch)
+    sp = np.sin(pitch)
+
+    cy = np.cos(yaw)
+    sy = np.sin(yaw)
+
+    return np.array([
+        [cp*cy, cp*sy, -sp],
+        [sr*sp*cy-cr*sy, sr*sp*sy+cr*cy, sr*cp],
+        [cr*sp*cy+sr*sy, cr*sp*sy-sr*cy, cr*cp]
+    ])
+
+# true_mat_3x3 = np.array([[True for i in range(3)] for j in range(3)])
+
+def test_get_dcm_ned2bod():
+
+    roll = np.random.uniform(low=-np.pi, high=np.pi)
+    pitch = np.random.uniform(low=-np.pi/2.0, high=np.pi/2.0)
+    yaw = np.random.uniform(low=-2.0*np.pi, high=2.0*np.pi)
+
+    print("\nroll = ", np.rad2deg(roll), " [deg]")
+    print("pitch = ", np.rad2deg(pitch), " [deg]")
+    print("yaw = ", np.rad2deg(yaw), " [deg]\n")
+
+    DCM_ned2bod = kt.get_dcm_ned2bod(roll, pitch, yaw)
+
+    DCM_ned2bod_explicit = get_dcm_ned2bod_explicit(roll, pitch, yaw)
+
+    print("DCM_ned2bod =\n", DCM_ned2bod)
+    print("DCM_ned2bod_explicit =\n", DCM_ned2bod_explicit)
+
+    assert DCM_ned2bod[0,0] == approx(DCM_ned2bod_explicit[0,0], abs=1.0e-6)
+    assert DCM_ned2bod[0,1] == approx(DCM_ned2bod_explicit[0,1], abs=1.0e-6)
+    assert DCM_ned2bod[0,2] == approx(DCM_ned2bod_explicit[0,2], abs=1.0e-6)
+    assert DCM_ned2bod[1,0] == approx(DCM_ned2bod_explicit[1,0], abs=1.0e-6)
+    assert DCM_ned2bod[1,1] == approx(DCM_ned2bod_explicit[1,1], abs=1.0e-6)
+    assert DCM_ned2bod[1,2] == approx(DCM_ned2bod_explicit[1,2], abs=1.0e-6)
+    assert DCM_ned2bod[2,0] == approx(DCM_ned2bod_explicit[2,0], abs=1.0e-6)
+    assert DCM_ned2bod[2,1] == approx(DCM_ned2bod_explicit[2,1], abs=1.0e-6)
+    assert DCM_ned2bod[2,2] == approx(DCM_ned2bod_explicit[2,2], abs=1.0e-6)
+
