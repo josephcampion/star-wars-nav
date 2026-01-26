5ac6870e261402bde4d4358ea10ea87bdc0a221f
Joey Campion <josephcampion@fastmail.com>
2026-01-25 19:55:56 -0500

    Add pitch control, including Nichols, Bode, step reponse

--- flight_controls/gains.py
+++ flight_controls/gains.py
@@ -25,8 +25,9 @@ ki_q = kp_q * omega_q
 # kd_q = 0.0
 
 #-------- Pitch Angle Control ------#
-kp_theta = 0.0
-ki_theta = 0.0
+kp_theta = 0.4
+omega_theta = 10.0
+ki_theta = kp_theta * omega_theta
 # kd_theta = 0.0
 
 # ------- Altitude Control --------#
@@ -49,4 +50,9 @@ ki_r = 0.0
 
 if __name__ == "__main__":
     print(f"kp_p = {kp_p}, ki_p = {ki_p}, omega_p = {omega_p}")
+    print(f"kp_phi = {kp_phi}, ki_phi = {ki_phi}, omega_phi = {omega_phi}")
     print(f"kp_q = {kp_q}, ki_q = {ki_q}, omega_q = {omega_q}")
+    print(f"kp_theta = {kp_theta}, ki_theta = {ki_theta}, omega_theta = {omega_theta}")
+    # print(f"kp_h = {kp_h}, ki_h = {ki_h}")
+    # print(f"kp_Va = {kp_Va}, ki_Va = {ki_Va}")
+    # print(f"kp_r = {kp_r}, ki_r = {ki_r}")
---
+++ flight_controls/pitch_control.py
@@ -0,0 +1,105 @@
+
+import numpy as np
+import control as ct # 3rd party
+import matplotlib.pyplot as plt
+import flight_controls.transfer_functions as tfs
+import flight_controls.utils as utils
+from flight_controls.utils import mag2db
+from models.actuator import tf_actuator
+import flight_controls.gains as gains
+
+
+####################################################
+#   Create Pitch Rate Inner Loop Transfer Function
+####################################################
+
+tf_de_to_q = tfs.tf_de_to_q 
+tf_de_cmd_to_q = tf_de_to_q * tf_actuator
+
+s = ct.tf('s')
+tf_q_ctrl = -1.0 * (gains.kp_q + gains.ki_q / s)
+# print(tf_q_ctrl)
+
+tf_q_closed_loop = (tf_q_ctrl * tf_de_cmd_to_q) / (1 + tf_q_ctrl * tf_de_cmd_to_q)
+# print(tf_q_closed_loop)
+
+
+####################################################
+                # TODO: Root Locus
+####################################################
+
+
+####################################################
+        #   Pitch Controller
+####################################################
+
+s = ct.tf('s')
+tf_theta_ctrl = gains.kp_theta + gains.ki_theta / s
+# print("tf_theta_ctrl = ", tf_theta_ctrl)
+
+tf_theta_open_loop = tf_theta_ctrl * tf_q_closed_loop
+# print("tf_theta_open_loop = ", tf_theta_open_loop)
+
+tf_theta_closed_loop = (tf_theta_ctrl * tf_q_closed_loop) / (1 + tf_theta_ctrl * tf_q_closed_loop)
+# print("tf_theta_closed_loop = ", tf_theta_closed_loop)
+
+
+####################################################
+# Plot open-loop Nichols for stability analysis
+####################################################
+
+fig, ax = plt.subplots()
+mag, phase_rad, omega = ct.bode(tf_theta_open_loop, plot=False)
+phase_deg = np.rad2deg(phase_rad)
+phase_deg = utils.wrap_phase(phase_deg)
+utils.plot_nichols(mag2db(mag), phase_deg, ax, label="Pitch Open-Loop")
+utils.plot_nichols_margins(ax)
+ax.set_title(r'$G_{ol}(s)=\frac{\theta(s)}{e_{\theta}(s)}=K_\theta(s)\cdot P_\theta(s)$')
+
+
+####################################################
+# Plot OPEN-loop Bode for control design
+####################################################
+
+fig, ax = plt.subplots(2,1)
+mag, phase_rad, omega = ct.bode(tf_theta_open_loop, plot=False)
+phase_deg = np.rad2deg(phase_rad)
+phase_deg = utils.wrap_phase(phase_deg)
+utils.plot_bode(mag2db(mag), phase_deg, omega, ax,label="Pitch Open-Loop")
+fig.suptitle(r'$G_{ol}(s)=\frac{\theta(s)}{e_{\theta}(s)}$')
+
+
+####################################################
+# Plot CLOSED-loop Bode for performance analysis
+####################################################
+
+fig, ax = plt.subplots(2,1)
+mag, phase_rad, omega = ct.bode(tf_theta_closed_loop, plot=False)
+phase_deg = np.rad2deg(phase_rad)
+phase_deg = utils.wrap_phase(phase_deg)
+utils.plot_bode(mag2db(mag), phase_deg, omega, ax,label="Pitch Closed-Loop")
+fig.suptitle(r'$G_{cl}(s)=\frac{\theta(s)}{\theta_{cmd}(s)}$')
+
+
+####################################################
+#   Closed-Loop Step Response
+#################################################### 
+
+dt = 1.e-3
+Tsim = 10.0
+timepts = np.arange(0, Tsim, dt)
+U = np.deg2rad(10.0) * np.ones([1,len(timepts)])
+response = ct.forced_response(tf_theta_closed_loop, timepts, U)
+t = response.time
+p_t = response.y[0,:]
+
+fig, ax = plt.subplots()
+ax.plot(t, np.rad2deg(p_t), label="Pitch Closed Loop")
+ax.set_title(r'$G_{cl}(s)=\frac{\theta(s)}{\theta_{cmd}(s)}$')
+ax.set_ylabel('[deg/s]')
+ax.set_xlabel('Time [s]')
+ax.grid(True)
+
+
+# Plot all the things!
+plt.show()
